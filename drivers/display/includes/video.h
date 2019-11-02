94e7e354b859710a4c3631d0426cb361534a4c6c
HamilcarR <bensalem759@hotmail.com>
2019-11-03 00:06:49 +0100

    added drivers

--- drivers/display/includes/video.h
+++ drivers/display/includes/video.h
@@ -8,6 +8,8 @@
 void video_write( unsigned char *string , char color);
 //clear the screen
 void clear_screen () ; 
+//write to position (COL , ROW) 
+void video_write_to(unsigned char* string , char color , uint16_t col , uint16_t row); 
 
 
 
